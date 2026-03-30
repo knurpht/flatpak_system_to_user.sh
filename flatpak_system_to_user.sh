c8591f64b5a747cce02bb220b72d917d4e77fd19
Gertjan Lettink <gertjan.lettink@glosscomputer.com>
2026-03-30 08:30:21 +0200

    More fixes and improvements, script now split into blocks for functions

--- flatpak_system_to_user.sh
+++ flatpak_system_to_user.sh
@@ -8,7 +8,10 @@
 # - run ./flatpak_system_to_user.sh as your user from that folder. 
 
 
+#
 # Set some vars 
+#
+# function set_vars() {
 FLATPAK_REMOTE_NAME="flathub"
 FLATPAK_REMOTE_URL="https://flathub.org/repo/flathub.flatpakrepo"
 FLATPAK_REMOTE_DISABLED="disabled"
@@ -16,51 +19,79 @@ FLATPAK_REMOTE_USER=$(flatpak --user remote-list --show-disabled| awk '{print $1
 FLATPAK_REMOTE_USER_DISABLED=$(flatpak --user remote-list --show-disabled | awk '{print $2}')
 FLATPAK_SYSTEM_COUNT=0
 FLATPAK_USER_COUNT=0
+# return 
+# }
 
+#
 # Check whether user is not "root"
-echo "Checking for permissions" >&2
+#
+# function check_root() {
+echo "Checking for user or root" >&2
 if [[ ! $EUID > 0 ]]; then
   echo "Run this as your user, exiting" >&2
   exit 1
+else 
+  echo "All well, you are not root" >&2
 fi
+# return 
+# }
 
+
+
+#
 # Install flathub remote to the user if needed
+#
+# function setup_remote() {
+echo "Checking for user's flathub remote" >&2
 if [[ ! $FLATPAK_REMOTE_USER ]]; then
-  echo "User flathub remote not installed" >&2
-  echo "Adding flathub remote for user" >&2
+  echo "- User's flathub remote not installed" >&2
+  echo "- Adding flathub remote for user" >&2
   flatpak --user remote-add --if-not-exists "$FLATPAK_REMOTE_NAME" "$FLATPAK_REMOTE_URL"
   sleep 5
+  echo "- User's flathub remote now installed" >&2  
   flatpak --user remote-modify --enable "$FLATPAK_REMOTE_NAME" >/dev/null 2>&1
-  echo "User flathub remote now enabled" >&2  
+  sleep 5
+  echo "- User's flathub remote now enabled" >&2  
 else 
-  echo "User flathub remote already installed" >&2
-  echo "Checking user flathub remote status" >&2 
+  echo "- User's flathub remote already installed" >&2
+  echo "Checking for user's flathub remote status" >&2 
   if [[ $FLATPAK_REMOTE_USER_DISABLED ]]; then
-    echo "User flathub remote disabled" >&2
+    echo "- User's flathub remote is disabled" >&2
     flatpak --user remote-modify --enable "$FLATPAK_REMOTE_NAME" >/dev/null 2>&1
-    echo "User flathub remote now enabled" >&2
+    sleep 5
+    echo "- User's flathub remote is now enabled" >&2
+  else
+    echo "- User's flathub remote already enabled" >&2   
   fi
-  echo "User flatpak remote setup complete !" >&2
+  echo "- User's flatpak remote setup complete !" >&2
 fi
+# return 
+# }
 
 
-# Create lists from both system and huser installed flatpaks
-echo "Create lists from system and user installed flatpaks" >&2
+
+#
+# Create lists from both system and user installed flatpaks
+#
+# function get_refs() {
+echo "Creating lists from system and user installed flatpaks" >&2
 mapfile -t system_fp < <(flatpak --system list --columns=ref | awk 'NF {print $1}')
 mapfile -t user_fp < <(flatpak --user list --columns=ref | awk 'NF {print $1}')
-
+# Build a set of system‑installed flatpak refs
+declare -A system_set
+for ref in "${system_fp[@]}"; do
+  system_set["$ref"]=1
+  ((FLATPAK_SYSTEM_COUNT++))
+done
+echo "- List for system's flatpak remote done" >&2
 # Build a set of user‑installed flatpak refs
 declare -A user_set
 for ref in "${user_fp[@]}"; do
   user_set["$ref"]=1
   ((FLATPAK_USER_COUNT++))
 done
-declare -A system_set
-for ref in "${system_fp[@]}"; do
-  system_set["$ref"]=1
-  ((FLATPAK_SYSTEM_COUNT++))
-done
-
+echo "- List for user's flatpak remote done" >&2
+# Create system installed flatpak counter
 OLD=$FLATPAK_SYSTEM_COUNT
 FP=""
 if [[ $OLD == 1 ]]; then
@@ -68,35 +99,45 @@ if [[ $OLD == 1 ]]; then
 else
   FP="flatpaks"
 fi
+# return 
+# }
 
+
+
+
+#
 # For each system flatpak, install in user if missing, then remove from system
-echo "Totals: To do: $OLD >> User: $FLATPAK_USER_COUNT" >&2
+#
+# function migrate() {
+echo "Totals: System: $OLD >> User: $FLATPAK_USER_COUNT" >&2
 if [[ ! $OLD == 0 ]]; then
   for ref in "${system_fp[@]}"; do
-    echo "Migrating $FLATPAK_SYSTEM_COUNT flatpaks to user install: $ref" >&2
+    echo "Migrating to user install: #$FLATPAK_SYSTEM_COUNT - $ref" >&2
     if [[ -n "${user_set[$ref]+x}" ]]; then 
       echo "- Already in user install: $ref" >&2
       echo "- Uninstalling flatpak from system: $ref" >&2
       sudo flatpak uninstall -y --system  --force-remove --noninteractive "$ref" >/dev/null 2>&1
       echo "- Uninstalled from system: $ref" >&2
       ((FLATPAK_SYSTEM_COUNT--))
-      echo "Totals: To do: $FLATPAK_SYSTEM_COUNT | User: $FLATPAK_USER_COUNT" >&2
+      echo "- To do: $FLATPAK_SYSTEM_COUNT | User: $FLATPAK_USER_COUNT" >&2
     else 
       echo "- Installing flatpak to user: $ref" >&2
       flatpak install -y --user --noninteractive flathub "$ref" >/dev/null 2>&1    
       echo "- Installed to user: $ref" >&2
       ((FLATPAK_USER_COUNT++))
-      echo "- Totals: To do: $FLATPAK_SYSTEM_COUNT | User: $FLATPAK_USER_COUNT" >&2
+      echo "- To do: $FLATPAK_SYSTEM_COUNT | User: $FLATPAK_USER_COUNT" >&2
       echo "- Uninstalling flatpak from system: $ref" >&2
       sudo flatpak uninstall -y --system --force-remove --noninteractive "$ref" >/dev/null 2>&1
       echo "- Uninstalled from system: $ref" >&2
       ((FLATPAK_SYSTEM_COUNT--))
-      echo "- Totals: To do: $FLATPAK_SYSTEM_COUNT | User: $FLATPAK_USER_COUNT" >&2
+      echo "- To do: $FLATPAK_SYSTEM_COUNT | User: $FLATPAK_USER_COUNT" >&2
     continue
     fi
   done
-  echo "Migration of $OLD $FP to user flathub remote completed" >&2
+  echo "Migration of $OLD $FP to user's flathub remote completed" >&2
 else 
   echo "Migration of $FLATPAK_SYSTEM_COUNT $FP to user is useless, exiting" >&2
   exit 1
 fi
+#}
+
